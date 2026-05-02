31416c3d82a4a87055017a5378e3d0ef3f133637
coderisetech2026-ship-it <coderisetech2026@gmail.com>
2026-05-02 14:59:09 +0530

    fix: rename test endpoint to -v2 to force new code usage

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -52,6 +52,6 @@ r.post('/inventory/reconcile', auditController.reconcileStock);
 // Settings & Config
 r.get('/settings', c.getSettings);
 r.put('/settings', c.updateSettings);
-r.post('/test-notifications', c.testNotifications);
+r.post('/test-notifications-v2', c.testNotifications);
 
 module.exports = r;
